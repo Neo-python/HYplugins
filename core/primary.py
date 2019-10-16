cb8a50eada6a18ee21da560cfb41752b95f78d01
neo <g602049338@icloud.com>
2019-10-17 03:05:19 +0800

    连接core

---
+++ core/primary.py
@@ -0,0 +1,65 @@
+"""中台相关"""
+import config
+import requests
+import json
+from flask import jsonify
+
+
+class CoreApi:
+    """核心中台接口"""
+    interface = f'http://127.0.0.1:{config.core_server_port}'
+
+    def send_sms(self, **kwargs):
+        """通知中台发送短信
+        :param kwargs:
+        :param kwargs: phone: str
+        :param kwargs: code: str
+        :param kwargs: template_id: str
+        """
+        interface_path = '/send_sms/code/'
+        url = f'{self.interface}{interface_path}'
+        result = requests.post(url, json=kwargs)
+        return self.understand(api_result=result)
+
+    def upload_url(self, **kwargs) -> dict:
+        """通知中通获取图片上传授权地址
+        :param kwargs:
+        :param kwargs: user_uuid:str
+        :param kwargs: genre:str
+        :param kwargs: suffix:str
+        :return:
+        """
+        interface_path = '/upload_url/'
+        url = f'{self.interface}{interface_path}'
+        result = requests.get(url, params=kwargs)
+        return self.understand(api_result=result)
+
+    def upload_credentials(self, **kwargs):
+        """通知中通获取图片上传授权地址
+                :param kwargs:
+                :param kwargs: user_uuid:str
+                :param kwargs: genre:str
+                :param kwargs: suffix:str
+                :return:
+                """
+        interface_path = '/upload_credentials/'
+        url = f'{self.interface}{interface_path}'
+        result = requests.get(url, params=kwargs)
+        return self.understand(api_result=result)
+
+    def get_open_id(self, **kwargs):
+        """获取open_id
+                :param kwargs:
+                :param kwargs: real_code:str
+                :return:
+                """
+        interface_path = '/get_open_id/'
+        url = f'{self.interface}{interface_path}'
+        result = requests.get(url, params=kwargs)
+        return self.understand(api_result=result)
+
+
+    @staticmethod
+    def understand(api_result) -> dict:
+        """处理接口响应"""
+        return jsonify(json.loads(api_result.content.decode()))
