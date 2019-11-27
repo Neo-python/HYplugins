9feb84ca9eca8819ce4fadd53840ecb43016a919
neo <g602049338@icloud.com>
2019-11-27 20:16:05 +0800

    管理员通知

--- core/primary.py
+++ core/primary.py
@@ -35,6 +35,7 @@ class CoreApi:
         """
         interface_path = '/send_sms/notice_manager/'
         url = f'{self.interface}{interface_path}'
+        print(kwargs)
         result = requests.post(url, json=kwargs)
         return self.understand(api_result=result)
 
